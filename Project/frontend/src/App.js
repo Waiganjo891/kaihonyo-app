fef76c82f2314294a5a2bd48c6795e70af43c1ee
Waiganjo891 <elijahthiongo34@gmail.com>
2021-04-03 10:09:45 +0300

    show code on github

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route} from "react-router-dom";
 import { signout } from "./actions/userActions.js";
+import AdminRoute from "./components/AdminRoute.js";
 import PrivateRoute from "./components/PrivateRoute.js";
 import CartScreen from "./screens/cartScreen.js";
 import HomeScreen from "./screens/HomeScreen.js";
@@ -9,6 +10,7 @@ import OrderHistoryScreen from "./screens/OrderHistoryScreens.js";
 import OrderScreen from "./screens/orderScreen.js";
 import PaymentMethodScreen from "./screens/PaymentMethod.js";
 import PlaceOrderScreen from "./screens/placeOrderScreen.js";
+import ProductListScreen from "./screens/productListScreens.js";
 import ProductScreen from "./screens/ProductScreen.js";
 import ProfileScreen from "./screens/profileScreen.js";
 import RegisterScreen from "./screens/Register Screen.js";
@@ -54,18 +56,18 @@ function App() {
                       )}
                     {userInfo && userInfo.isAdmin && (
               <div className="dropdown">
-                <Link to="#admin">
+                <Link className="head" to="#admin">
                   Admin <i className="fa fa-caret-down"></i>
                 </Link>
                 <ul className="dropdown-content">
                   <li>
-                    <Link to="/productlist">Products</Link>
+                    <Link className="head" to="/productlist">Products</Link>
                   </li>
                   <li>
-                    <Link to="/orderlist">Orders</Link>
+                    <Link className="head" to="/orderlist">Orders</Link>
                   </li>
                   <li>
-                    <Link to="/userlist">Users</Link>
+                    <Link className="head" to="/userlist">Users</Link>
                   </li>
                 </ul>
               </div>
@@ -82,6 +84,7 @@ function App() {
               <PrivateRoute path="/profile" component={ProfileScreen}></PrivateRoute>
               <PrivateRoute path="/payment" component={PaymentMethodScreen}></PrivateRoute>
               <PrivateRoute path="/order/:id" component={OrderScreen}></PrivateRoute>
+              <AdminRoute path="/productlist" component={ProductListScreen}></AdminRoute>
               <Route path="/" component={HomeScreen} exact></Route>
             </main>
             <footer className="row center">
